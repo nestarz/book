ae33a83d4ef0b54cda77c0d128f4bc6d8037939e
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2018-10-16 05:28:48 +0200

    fix broken md images

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,16 @@ module.exports = {
           {
             resolve: 'gatsby-remark-responsive-iframe',
           },
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true //Optional: Disable insertion of <style> border: 0
+            }
+          },
         ],
       },
     },
@@ -88,22 +98,5 @@ module.exports = {
     },
     'gatsby-plugin-offline',
     'gatsby-plugin-netlify',
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-        {
-          resolve: "gatsby-remark-embed-video",
-          options: {
-            width: 800,
-            ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-            height: 400, // Optional: Overrides optional.ratio
-            related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
-            noIframeBorder: true //Optional: Disable insertion of <style> border: 0
-          }
-        }
-        ]
-      }
-    },
   ],
 };
