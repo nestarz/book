717afd6845757e92929282db790b2be7e5f43cab
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2018-11-19 16:39:33 +0100

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,14 +21,14 @@ pointer-events: none;
 z-index:-1;
 `;
 
-const StyledSketch = styled(SketchComponent)`
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  pointer-events: none;
-  z-index: 1000;
-  mix-blend-mode: screen;
-`;
+// const StyledSketch = styled(SketchComponent)`
+//   position: fixed;
+//   top: 0;
+//   bottom: 0;
+//   pointer-events: none;
+//   z-index: 1000;
+//   mix-blend-mode: screen;
+// `;
 
 const IndexWrapper = styled.div`
 flex: 1;
