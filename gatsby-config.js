3afcbc2b3b5dadedfc8a3da9c748a0fedd671e9d
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-01-13 16:50:58 +0100

    add gif, pdf, ... support with gatsby-remark-copy-linked-files

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,6 +45,10 @@ module.exports = {
             },
           },
           {
+            resolve: "gatsby-remark-copy-linked-files",
+            options: {}
+          },
+          {
             resolve: 'gatsby-remark-toc',
             options: {
               mdastUtilTocOptions: {
