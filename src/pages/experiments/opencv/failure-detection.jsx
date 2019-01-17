4a8e6838902877dcf31bf3d42a301f1de6a2fd03
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-01-17 05:14:27 +0100

    update

--- src/pages/experiments/opencv/failure-detection.jsx
+++ src/pages/experiments/opencv/failure-detection.jsx
@@ -1,6 +1,34 @@
 import React, { useRef, useState } from 'react';
+import Layout from 'components/Layout';
 import OpenCvWebcam from "components/OpenCV/Webcam";
-import startOpenCvProgram from "components/OpenCV/basic";
+import startOpenCvProgram from "components/OpenCV/background_substraction";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+display: flex;
+flex: 1;
+button {
+    background: none;
+    color: inherit;
+    border: none;
+    padding: 0;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+    text-decoration: underline;
+}
+`;
+const Canvas = styled.canvas`
+position: fixed;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+width: 100vw;
+height: 100vh;
+mix-blend-mode: difference;
+pointer-events: none;`;
+
 
 const Index = () => {
     let canvasOutputRef = useRef(),
@@ -9,17 +37,19 @@ const Index = () => {
     let [stopped, setStopped] = useState(false);
 
     return (
-        <>
-            <OpenCvWebcam
-                ref={OpenCvWebcamRef}
-                startOpenCvProgram={startOpenCvProgram}
-                canvasOutputRef={canvasOutputRef}
-                buttonToggleStopRef={buttonToggleStopRef}
-                webcamWidth={640}
-            />
-            <canvas ref={canvasOutputRef} />
-            <button ref={buttonToggleStopRef} onClick={() => setStopped(!stopped)}>{stopped ? "Démarrer" : "Stopper"}</button>
-        </>
+        <Layout>
+            <Wrapper>
+                <OpenCvWebcam
+                    ref={OpenCvWebcamRef}
+                    startOpenCvProgram={startOpenCvProgram}
+                    canvasOutputRef={canvasOutputRef}
+                    buttonToggleStopRef={buttonToggleStopRef}
+                    webcamWidth={640}
+                />
+                <Canvas ref={canvasOutputRef} />
+                <button ref={buttonToggleStopRef} onClick={() => setStopped(!stopped)}>{stopped ? "Démarrer" : "Stopper"}</button>
+            </Wrapper>
+        </Layout>
     )
 };
 
