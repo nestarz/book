8634cf7bfe4af14187ffd748ff65c2ac0f0789c2
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-01-17 15:57:26 +0100

    update

--- src/pages/experiments/opencv/failure-detection.jsx
+++ src/pages/experiments/opencv/failure-detection.jsx
@@ -4,9 +4,25 @@ import OpenCvWebcam from "components/OpenCV/webcam";
 import startOpenCvProgram from "components/OpenCV/background_substraction";
 import styled from 'styled-components';
 
+const Hover = styled.div`
+position: fixed;
+top: 0;
+bottom: 0;
+left: 0;
+right: 0;
+width: 100vw;
+height: 100vh;
+mix-blend-mode: screen;
+pointer-events: none;
+mix-blend-mode: color;
+background-color: darkblue;
+`
 const Wrapper = styled.div`
 display: flex;
 flex: 1;
+justify-content: center;
+align-items: center;
+flex-direction: column;
 button {
     background: none;
     color: inherit;
@@ -18,16 +34,8 @@ button {
     text-decoration: underline;
 }
 video {
-    position: fixed;
-top: 0;
-bottom: 0;
-left: 0;
-right: 0;
-width: 100vw;
-height: 100vh;
-mix-blend-mode: difference;
-pointer-events: none;
-filter: saturate(0);
+    filter: invert(1) blur(0px);
+
 }
 `;
 const Canvas = styled.canvas`
@@ -39,7 +47,10 @@ right: 0;
 width: 100vw;
 height: 100vh;
 mix-blend-mode: multiply;
-pointer-events: none;`;
+pointer-events: none;
+filter: invert(1) blur(80px);
+/*filter: invert(100);*/
+`;
 
 
 const Index = () => {
@@ -56,11 +67,12 @@ const Index = () => {
                     startOpenCvProgram={startOpenCvProgram}
                     canvasOutputRef={canvasOutputRef}
                     buttonToggleStopRef={buttonToggleStopRef}
-                    webcamWidth={640}
+                    webcamWidth={400}
                 />
                 <Canvas ref={canvasOutputRef} />
                 <button ref={buttonToggleStopRef} onClick={() => setStopped(!stopped)}>{stopped ? "Démarrer" : "Stopper"}</button>
             </Wrapper>
+            <Hover/>
         </Layout>
     )
 };
