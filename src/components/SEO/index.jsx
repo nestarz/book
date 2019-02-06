94ddb824a3ead37f140fc0368327c2a0a8143cd4
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-02-06 12:58:50 +0100

    update favicons

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -154,7 +154,6 @@ const Head = props => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="image" content={image} />
-      <meta name="gatsby-starter" content="Gatsby Starter Portfolio Emma" />
       <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
