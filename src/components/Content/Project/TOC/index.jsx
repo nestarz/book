913fb7d0ec97f58756b011e57a1c448c4b0f885d
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-02-07 02:39:47 +0100

    bg from visit card

--- src/components/Content/Project/TOC/index.jsx
+++ src/components/Content/Project/TOC/index.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { Wrapper, Content } from './styles'
-import { SketchComponentAbsoluteBackground } from 'components/Visual/P5js';
-import backgroundSketch from 'components/Visual/P5js/projects/mainScreen/sketch1_0';
 import SpringPosition from 'components/Layout/SpringPosition';
+import { SketchComponentAbsoluteBackground } from 'components/Visual/P5js';
+import * as sketcheModules2 from 'components/Visual/P5js/projects/visit-card';
+import React from 'react';
 import { animated } from 'react-spring';
-import * as sketcheModules from 'components/Visual/P5js/projects/project-header';
+import { Content, Wrapper } from './styles';
 
-const sketches = [backgroundSketch, ...Object.values(sketcheModules)];
+const sketches = [...Object.values(sketcheModules2)];
 const randomSelect = a => a[Math.floor(Math.random() * a.length)];
 const sketchSelection = (sketch, sketchIndex) => sketch ? sketch :
 ((sketchIndex || sketchIndex == 0) ? sketches[sketchIndex % sketches.length] : randomSelect(sketches))
