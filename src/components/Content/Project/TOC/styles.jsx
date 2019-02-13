e92a4c372a8dbdb3f8b4b9b78e109452f43783e3
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-02-13 14:11:04 +0100

    fix toc

--- src/components/Content/Project/TOC/styles.jsx
+++ src/components/Content/Project/TOC/styles.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   flex: 1;
   display: flex;
-  flex-wrap: wrap-reverse;
   canvas {
     max-height: 100vh !important;
   }
